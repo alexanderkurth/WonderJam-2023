568047816123213a984a12860b63c12d01117176
TheoG <theogimenez7@gmail.com>
2023-02-25 02:04:08 -0500

    Adding shaking tick on manager

--- CavalierBlingUnity/Assets/Scripts/Madness/mMadnessManager.cs
+++ CavalierBlingUnity/Assets/Scripts/Madness/mMadnessManager.cs
@@ -2,23 +2,29 @@ using UnityEngine;
 class mMadnessManager : MonoBehaviour
 {
     [SerializeField]
-    private int madnessCurrentLevel = 0;
+    private int m_MadnessCurrentLevel = 0;
     [SerializeField]
-    private const int MADNESSMAXLEVEL = 100;
+    private const int m_MadnessMaxLevel = 100;
+
+    [SerializeField]
+    private const int m_StartShakingLevel = 50;
+
+    [SerializeField]
+    private const int m_MaxShakingIntensity = 50;
     
     public int GetCurrentMadnessLevel()
     {
-        return madnessCurrentLevel;
+        return m_MadnessCurrentLevel;
     }
     public int GetMaxMadnessLevel()
     {
-        return MADNESSMAXLEVEL;
+        return m_MadnessMaxLevel;
     }
 
     public void IncreaseMadnessLevel(int amount = 1)
     {
-        madnessCurrentLevel += amount;
-        if (madnessCurrentLevel > MADNESSMAXLEVEL)
+        m_MadnessCurrentLevel += amount;
+        if (m_MadnessCurrentLevel > m_MadnessMaxLevel)
         {
             // TODO : ADD GAME OVER
             Debug.Log("GAME OVER");
@@ -27,10 +33,20 @@ class mMadnessManager : MonoBehaviour
 
     public void ReduceCurrentLevel(int amount = 1)
     {
-        madnessCurrentLevel -= amount;
-        if (madnessCurrentLevel < 0)
+        m_MadnessCurrentLevel -= amount;
+        if (m_MadnessCurrentLevel < 0)
         {
-            madnessCurrentLevel = 0;
+            m_MadnessCurrentLevel = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_MadnessCurrentLevel > m_StartShakingLevel)
+        {
+            // Should be between 0 and m_MaxShakingIntensity
+            int cameraShakingIntensity = ((m_MadnessCurrentLevel - m_StartShakingLevel) / (m_MadnessMaxLevel - m_StartShakingLevel)) * m_MaxShakingIntensity;
+            // TODO : Camera.Shake(cameraShakingIntensity);
         }
     }
 }
