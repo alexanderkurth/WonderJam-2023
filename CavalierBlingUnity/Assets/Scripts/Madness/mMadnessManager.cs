9270e8a60f4d0aadff3c5eedf90c4665a301db9e
TheoG <theogimenez7@gmail.com>
2023-02-25 00:56:17 -0500

    Adding scene and fix code

--- CavalierBlingUnity/Assets/Scripts/Madness/mMadnessManager.cs
+++ CavalierBlingUnity/Assets/Scripts/Madness/mMadnessManager.cs
@@ -1,5 +1,19 @@
+using UnityEngine;
 // Make a manager for the madness system
 class mMadnessManager : MonoBehaviour
 {
+    [SerializeField]
+    private int madnessCurrentLevel = 0;
+    [SerializeField]
+    private const int MADNESSMAXLEVEL = 100;
     
+    public void ReduceCurrentLevel(int amount = 1)
+    {
+        madnessCurrentLevel -= amount;
+        if (madnessCurrentLevel < 0)
+        {
+            // TODO : ADD GAME OVER
+            madnessCurrentLevel = 0;
+        }
+    }
 }
