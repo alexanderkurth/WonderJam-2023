58336f1806e5398f13f6fd9c6b9ce1e940f105cc
JPatrat <jeffrey.patrat@laposte.net>
2023-02-25 17:45:08 -0500

    *UpdateShop

--- CavalierBlingUnity/Assets/Scripts/Shop/ShopSceneManager.cs
+++ CavalierBlingUnity/Assets/Scripts/Shop/ShopSceneManager.cs
@@ -22,8 +22,6 @@ public class ShopSceneManager : AbstractSingleton<ShopSceneManager>
     [SerializeField]
     private Vector2 _mHesitationTimePerObject = new Vector2(0.25f, 0.5f);
     [SerializeField]
-    private int _mPercentChanceChoseItem = 20;
-    [SerializeField]
     private float _mSpendingMoveToStartTime = 2f;
     [SerializeField]
     private float _mNoSpendingMoveToStartTime = 4f;
