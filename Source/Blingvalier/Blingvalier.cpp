75981ea20357a8a908d188363c49e818729f4be1
TibRib <meta-block@hotmail.fr>
2023-02-25 00:01:54 -0500

    rm all

--- Source/Blingvalier/Blingvalier.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Blingvalier.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Blingvalier, "Blingvalier" );
- 
