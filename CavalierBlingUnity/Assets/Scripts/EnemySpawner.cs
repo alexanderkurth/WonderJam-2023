0bdd89c058d8970b3ef0a4af8e65dfcaeee183cb
StienVdP <32348262+StienVdP@users.noreply.github.com>
2023-02-25 13:53:38 -0500

    Make StartSpawn function for GameMode

--- CavalierBlingUnity/Assets/Scripts/EnemySpawner.cs
+++ CavalierBlingUnity/Assets/Scripts/EnemySpawner.cs
@@ -11,21 +11,21 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private Transform m_PivotTransform;
     [SerializeField]
-    private int m_nbEnemy;
-    [SerializeField]
     private float m_DeltaTime;
     [SerializeField]
     private float m_SpawnAngle = 45.0f;
     [SerializeField]
     private int m_nbSpawnedPosition = 5;
+    private int m_nbEnemy;
 
-
-    void Start()
+    // To be called by GameMode
+    public void StartSpawn(int nbSpawnedEntity)
     {
-        StartCoroutine(SurroundStepAnimated());
+        m_nbEnemy = nbSpawnedEntity;
+        StartCoroutine(Spawn());
     }
 
-    IEnumerator SurroundStepAnimated()
+    IEnumerator Spawn()
     {
         yield return new WaitForSeconds(m_DeltaTime);
 
@@ -34,7 +34,7 @@ public class EnemySpawner : MonoBehaviour
         for (int i = 1; i < m_nbEnemy; i++)
         {
             int position = Random.Range(0, m_nbSpawnedPosition);
-            GameObject newSurrounderObject = Instantiate(m_EnemyPrefab, m_PivotTransform.position, Quaternion.identity, transform);
+            GameObject newSurrounderObject = Instantiate(m_EnemyPrefab, m_PivotTransform.position, Quaternion.identity, null);
 
             float angle = (AngleStep * position) - (m_SpawnAngle / 2.0f);
 
@@ -46,45 +46,4 @@ public class EnemySpawner : MonoBehaviour
             yield return new WaitForSeconds(m_DeltaTime);
         }
     }
-
-
-
-
-    /*
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_SpawnPosition = this.transform.position;
-
-        EnemyMove enemy = m_EnemyPrefab.GetComponent<EnemyMove>();
-        enemy.m_Target = m_TargetPrefab;
-
-        StartCoroutine(RequestSpawn());
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
-    }
-
-    IEnumerator RequestSpawn()
-    {
-        for (int i = 0; i < m_nbEnemy; i++)
-        {
-            yield return StartCoroutine(SpawnEnemy());
-
-            if (i == m_nbEnemy - 1)
-            {
-                m_nbEnemy = 0;
-            }
-        }
-    }
-
-    IEnumerator SpawnEnemy()
-    {
-        yield return new WaitForSeconds(m_DeltaTime);
-
-        Instantiate(m_EnemyPrefab, m_SpawnPosition, Quaternion.identity);
-    }*/
 }
