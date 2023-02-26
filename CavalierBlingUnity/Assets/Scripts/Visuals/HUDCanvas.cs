0e3b7de4e1963a47cc65775ac257a18886583080
JPatrat <jeffrey.patrat@laposte.net>
2023-02-26 00:02:15 -0500

    +newCanvas

---
+++ CavalierBlingUnity/Assets/Scripts/Visuals/HUDCanvas.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDCanvas : AbstractSingleton<HUDCanvas>
+{
+    [SerializeField]
+    private GameObject _mMessageObject = null;
+}
